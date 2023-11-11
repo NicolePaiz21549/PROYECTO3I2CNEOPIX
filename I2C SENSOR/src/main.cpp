393abba9cc36f0a94d09d2316daef84996a95aa4
NicolePaiz21549 <84263290+NicolePaiz21549@users.noreply.github.com>
2023-11-11 16:18:39 -0600

    Update main.cpp

--- I2C SENSOR/src/main.cpp
+++ I2C SENSOR/src/main.cpp
@@ -1,32 +1,6 @@
 #include <Arduino.h>
-/*
-  Optical Heart Rate Detection (PBA Algorithm) using the MAX30105 Breakout
-  By: Nathan Seidle @ SparkFun Electronics
-  Date: October 2nd, 2016
-  https://github.com/sparkfun/MAX30105_Breakout
-
-  This is a demo to show the reading of heart rate or beats per minute (BPM) using
-  a Penpheral Beat Amplitude (PBA) algorithm.
-
-  It is best to attach the sensor to your finger using a rubber band or other tightening
-  device. Humans are generally bad at applying constant pressure to a thing. When you
-  press your finger against the sensor it varies enough to cause the blood in your
-  finger to flow differently which causes the sensor readings to go wonky.
-
-  Hardware Connections (Breakoutboard to Arduino):
-  -5V = 5V (3.3V is allowed)
-  -GND = GND
-  -SDA = A4 (or SDA)
-  -SCL = A5 (or SCL)
-  -INT = Not connected
-
-  The MAX30105 Breakout can handle 5V or 3.3V I2C logic. We recommend powering the board with 5V
-  but it will also run at 3.3V.
-*/
-
 #include <Wire.h>
 #include "MAX30105.h"
-
 #include "heartRate.h"
 
 MAX30105 particleSensor;
@@ -35,25 +9,30 @@ const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
 byte rates[RATE_SIZE]; //Array of heart rates
 byte rateSpot = 0;
 long lastBeat = 0; //Time at which the last beat occurred
-
-float beatsPerMinute;
-int beatAvg;
+float beatsPerMinute; 
+int beatAvg; //Valor BPM 
 
 void setup()
 {
   Serial.begin(115200);
-  Serial.println("Initializing...");
+  Serial.println("Inicializando...");
 
   // Initialize sensor
   if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
   {
-    Serial.println("MAX30105 was not found. Please check wiring/power. ");
+    Serial.println("No se encontró el sensor MAX30105. Por favor revisar cableado y/o alimentación. ");
     while (1);
   }
-  Serial.println("Place your index finger on the sensor with steady pressure.");
-
-  particleSensor.setup(); //Configure sensor with default settings
-  particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
+  Serial.println("Coloca tu dedo índice sobre el sensor con presión constante.");
+  byte ledBrightness = 60; //Brillo al máximo donde las opciones son: 0=OFF
+  byte sampleAverage = 4;
+  byte ledMode = 2; //Donde las opciones son: 1 = ROJO, 2 = ROJO + IR, 3 = ROJO + IR + VERDE
+  byte sampleRate = 100; 
+  int pulseWidth = 411; //Detección de latidos normales
+  int adcRange = 4096; 
+  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with default settings
+  //particleSensor.setPulseAmplitudeRed(0x1F); //Máxima amplitud para el LED rojo
+  //particleSensor.setPulseAmplitudeIR(0x1F); //Máxima amplitud para el LED infrarrojo
   particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
 }
 
@@ -80,6 +59,7 @@ void loop()
         beatAvg += rates[x];
       beatAvg /= RATE_SIZE;
     }
+    delay(50); //Para evitar saturación en todos los sentidos
   }
 
   Serial.print("IR=");
